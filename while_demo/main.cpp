6d843758451c47cf79b3e661abf0a25f29904bb8
Edwin-1982 <shiqin7@hotmail.com>
2019-11-28 20:51:46 +0800

    增加while_demo项目

---
+++ while_demo/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<windows.h>
+#include<string>
+
+using namespace std;
+
+int main(void) {
+	int i;
+	int s;
+
+	s = 1;
+	i = 2;//i表示加数
+	// i
+	//1+2+3+...+100=?
+	//加99次
+
+	while (i<=100) {
+		
+		s = s + i;
+		i++;        //i=i+1 改变循环控制变量
+	}
+	cout << "s=" << s << endl;
+	system("pause");
+	return 0;
+}

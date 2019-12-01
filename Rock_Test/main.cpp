fab68559bbc9a573287a24cb0e115bb623c9df52
Edwin-1982 <shiqin7@hotmail.com>
2019-12-01 21:36:01 +0800

    增加"客户端"项目

--- Rock_Test/main.cpp
+++ Rock_Test/main.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 int main(void) {
 
-	cout << "Hello World!" << endl;
+	string pwd;
+	while (1) {
+		cout << "Please input your password:";
+		cin >> pwd;
+
+		if (pwd == "000123") {
+			break;
+		}
+		else {
+			cout << "Passeword error." << endl;
+		}
+	}
+	cout << "login success!" << endl;
+	cout << "1.×¢²á" << endl;
+	cout << "2.¹ÜÀí" << endl;
+	cout << "3.²éÑ¯" << endl;
+	cout << "4.É¾³ý" << endl;
 	system("pause");
 	return 0;
 }

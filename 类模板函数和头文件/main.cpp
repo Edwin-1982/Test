06c7b73b8ccdb1c55d42480c9a64e629e8b9ca84
Edwin-1982 <shiqin7@hotmail.com>
2020-04-07 12:59:17 +0800

    2020.04.06 12:59

--- 类模板函数和头文件/main.cpp
+++ 类模板函数和头文件/main.cpp
@@ -1,29 +1,5 @@
 #include<iostream>
-#include"demo.h"
-
-using namespace std;
-
-template<typename T>
-A<T>::A(T t) {
-	this->t = t;
-}
-
-//成员函数返回值使用虚拟类型
-template<typename T>
-T& A<T>::getT() {
-	return t;
-}
-
-template<typename T>
-A<T> A<T>::operator + (const A<T> &other) {
-	A tmp; //类的内部类型,可以显示声明也可以不显示
-	tmp.t = this->t + other.t;
-	return tmp;
-}
-template<typename T>
-void A<T>::print() {
-	cout << this->t << endl;
-}
+#include"demo.hpp"
 
 int main(void) {
 	A<int> a(666), b(888);

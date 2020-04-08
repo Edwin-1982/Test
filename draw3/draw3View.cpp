8dd75897cfe28fac75bcb581e68befa58a64acda
Edwin-1982 <shiqin7@hotmail.com>
2020-04-08 14:44:00 +0800

    2020.04.08 14:43

---
+++ draw3/draw3View.cpp
@@ -0,0 +1,113 @@
+﻿
+// draw3View.cpp: Cdraw3View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "draw3.h"
+#endif
+
+#include "draw3Doc.h"
+#include "draw3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Cdraw3View
+
+IMPLEMENT_DYNCREATE(Cdraw3View, CView)
+
+BEGIN_MESSAGE_MAP(Cdraw3View, CView)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+// Cdraw3View 构造/析构
+
+Cdraw3View::Cdraw3View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+	m_pOrigin = 0;
+}
+
+Cdraw3View::~Cdraw3View()
+{
+}
+
+BOOL Cdraw3View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Cdraw3View 绘图
+
+void Cdraw3View::OnDraw(CDC* /*pDC*/)
+{
+	Cdraw3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// Cdraw3View 诊断
+
+#ifdef _DEBUG
+void Cdraw3View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void Cdraw3View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+Cdraw3Doc* Cdraw3View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Cdraw3Doc)));
+	return (Cdraw3Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Cdraw3View 消息处理程序
+
+
+void Cdraw3View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	//保存起点到成员变量
+	m_pOrigin = point;
+	int a = 1;
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void Cdraw3View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	//画线
+	//获取上下文设备
+	CDC* pDC = GetDC();
+	
+	//画笔移动到起始点
+	pDC->MoveTo(m_pOrigin);
+	//从起点到终点画一条线
+	pDC->LineTo(point);
+
+	ReleaseDC(pDC);
+
+	CView::OnLButtonUp(nFlags, point);
+}
---
+++ draw3/draw3View.h
@@ -0,0 +1,51 @@
+﻿
+// draw3View.h: Cdraw3View 类的接口
+//
+
+#pragma once
+
+
+class Cdraw3View : public CView
+{
+protected: // 仅从序列化创建
+	Cdraw3View() noexcept;
+	DECLARE_DYNCREATE(Cdraw3View)
+
+// 特性
+public:
+	Cdraw3Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~Cdraw3View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+protected:
+	CPoint m_pOrigin;
+};
+
+#ifndef _DEBUG  // draw3View.cpp 中的调试版本
+inline Cdraw3Doc* Cdraw3View::GetDocument() const
+   { return reinterpret_cast<Cdraw3Doc*>(m_pDocument); }
+#endif
+

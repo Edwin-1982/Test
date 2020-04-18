ab0e57ebfd18ae3c0018b495501f5ed8699d5a26
Edwin-1982 <shiqin7@hotmail.com>
2020-04-18 15:13:56 +0800

    异常处理机制

--- C++异常处理机制/main.cpp
+++ C++异常处理机制/main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int copyfile2(const char* dest, const char* src) {
 	FILE* fp1 = NULL, * fp2 = NULL;
 
+	//通过throw操作创建一个异常对象并抛掷
+	throw 0.01f;
 	//rb 只读方式打开一个二进制文件,只允许读取数据
 	fopen_s(&fp1, src, "rb");
 
@@ -40,21 +42,45 @@ int copyfile2(const char* dest, const char* src) {
 	return 0;
 }
 int copyfile1(const char* dest, const char* src) {
-	return copyfile2(dest, src);
+	try {
+		copyfile2(dest, src);
+	}
+	catch(float e){
+		//throw;
+		printf("copyfile1-catch...\n");
+		throw e;
+	}
+	return 0;
 }
 int main(void) {
-
 	int ret = 0;
-	try {
+
+	//在需要捕捉异常的地方,将可能抛出异常的程序段嵌在try块之中
+	//按正常的程序顺序执行到达try语句,然后执行try块{}内的保护段
+	//如果在保护段执行期间没有引起异常,那么跟在try块后的catch子句就不执行,程序从try块后跟随的最后一个catch子句
+
+	try {//保护段
+		printf("开始执行copyfile1...\n");
 		ret = copyfile1("E:/迅雷下载/Git版本库/Test/C++异常处理机制/dest.txt", "E:/迅雷下载/Git版本库/Test/C++异常处理机制/src.txt");
+		printf("执行copyfile1完毕!\n");
 	}
+	//catch子句按其在try块后出现的顺序被检查,匹配的catch子句将捕获并按catch子句中的代码处理
 	catch (int error) {
-		printf("出现异常!%d\n",error);
+		printf("出现异常!%d\n", error);
 	}
-	catch (string *error) {
-		printf("捕捉到字符串异常:%s\n", error->c_str());
+	catch (string* error) {
+		printf("捕捉到字符串异常!%s\n", error->c_str());
 		delete error;
 	}
+	catch (float error) {
+		printf("出现异常!%f\n", error);
+	}
+	catch (...) {
+		printf("catch...\n");
+	}
+
+	//如果没有找到匹配,则缺省功能是调用abort终止程序
+
 	system("pause");
 	return 0;
 }
